940ce1d3815412997bcd5f22025cef6e307dca3f
Jeremy Murphy <jeremy@massively.ai>
2019-01-29 13:18:41 -0500

    commiting adding song component and random song data to body

--- vue_project/src/components/Body.vue
+++ vue_project/src/components/Body.vue
@@ -1,12 +1,47 @@
 <template>
     <div>
-       <h1> Something will go here</h1>
+       <h1> Songs in the Playlist</h1>
+       <div v-bind:key="song.id" v-for="song in songs">
+          <Song v-bind:song="song" />
+       </div>
     </div>
 </template>
 
+
+
 <script>
+import Song from '../components/Song.vue'
 export default {
   name: 'Body',
+  components: {
+    Song    
+  },
+  data(){
+    return {
+      songs: [
+        {
+          id: 1,
+          song: 'La Macarena',
+          artist: 'Los Del Rio'
+        },
+        {
+          id: 2,
+          song: 'Never gonna give you up',
+          artist: 'Rick Astley'
+        },
+        {
+          id: 3,
+          song: 'Who let the dogs out',
+          artist: 'Baha Men'
+        },
+        {
+          id: 4,
+          song: 'Back in Black',
+          artist: 'AC/DC'
+        }
+      ]
+    }
+  }
 }
 </script>
 
---
+++ vue_project/src/components/Song.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{song.song}}</h1>
+        <h3> {{song.artist}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Song',
+  props: {
+      song: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
